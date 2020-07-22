82cf4e0e06a3488639a4756909d9e73430ab2d10
muralikarthikk <muralikarthik.k123@gmail.com>
2020-07-22 16:32:55 +0000

    setting the base url for the notebook

--- docker/helx-hail/entrypoint.sh
+++ docker/helx-hail/entrypoint.sh
@@ -2,8 +2,9 @@
 
 . /opt/conda/etc/profile.d/conda.sh
 JUPYTER_PORT=$(cat /jupyter_port.ini)
+NB_PREFIX=${NB_PREFIX}
 export PATH=/opt/conda/bin/:$PATH
 conda init bash
 conda activate hail
 python -m ipykernel install --user --name hail --display-name "Hail-Python3"
-jupyter lab --ip=0.0.0.0 --port=$JUPYTER_PORT --LabApp.token=
+jupyter lab --ip=0.0.0.0 --port=$JUPYTER_PORT --LabApp.token= --NotebookApp.base_url=${NB_PREFIX} --NotebookApp.allow_origin="*"

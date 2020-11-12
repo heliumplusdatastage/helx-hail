43c29ee1d3e10967b5ec1b6a16e7dd4e11449356
Mani Singh <singh@rciadmins-MacBook-Pro.local>
2020-11-12 17:35:34 -0500

    Install ipykernel in hail env

--- docker/helx-hail/entrypoint.sh
+++ docker/helx-hail/entrypoint.sh
@@ -6,5 +6,6 @@ NB_PREFIX=${NB_PREFIX}
 export PATH=/opt/conda/bin/:$PATH
 conda init bash
 conda activate hail
-python -m ipykernel install --user --name hail --display-name "Hail-Python3"
+python3 -m ipykernel install --user --name hail --display-name "Hail-Python3"
 jupyter lab --ip=0.0.0.0 --port=$JUPYTER_PORT --LabApp.token= --NotebookApp.base_url=${NB_PREFIX} --NotebookApp.allow_origin="*"
+#jupyter lab --ip=0.0.0.0 --port=$JUPYTER_PORT --LabApp.token= --NotebookApp.allow_origin="*"

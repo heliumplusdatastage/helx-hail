12dc142ccfebbd9d5287a7506eda51af4a0d1901
Mani Singh <singh@rciadmins-MBP.attlocal.net>
2020-11-13 10:56:17 -0500

    removed obsolete subdirectories

--- docker/jupyter-hail/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-JUPYTER_PORT=$(cat ~/jupyter_port.ini)
-. ~/hail/bin/activate
-python -m ipykernel install --user --name hail --display-name "Hail-Python3"
-jupyter lab --ip=0.0.0.0 --port=$JUPYTER_PORT --LabApp.token=
